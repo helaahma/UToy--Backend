eb8dae3b9e154a2a028fdbc4eba42202f24bdc1f
Nawaf ALrifai <nbalrifai@gmail.com>
2019-10-05 21:48:56 +0300

    sell request

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (CollectableList, CollectableDetails, UserCreateAPIView)
+from .views import (CreateSellRequest,RequestUpdateView,CollectableList, CollectableDetails, UserCreateAPIView)
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 
@@ -8,6 +8,8 @@ path('login/', TokenObtainPairView.as_view() , name='login'),
 path('register/', UserCreateAPIView.as_view(), name='register'),
 path('collectable/list/', CollectableList.as_view(), name='collectable-list'),
 path('collectable/detail/<int:collectable_id>/', CollectableList.as_view(), name='collectable-detail'),
-path('SellRequest/', SellRequestSerializer.as_view(), name='sell-request'),
+path('sellrequest/', CreateSellRequest.as_view(), name='sell-request'),
+path('sellrequest/update/<int:sellrequest_id>', RequestUpdateView.as_view(), name='update-sellrequest'),
 
 ]
+

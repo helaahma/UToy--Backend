fa1e304d1f5c127d5d237aafc9e07b7cdb620ab7
Ahmad Helal <you@example.com>
2019-10-07 19:19:40 +0300

    cart- may be used later

--- api/views.py
+++ api/views.py
@@ -48,3 +48,77 @@ class RequestUpdateView(RetrieveUpdateAPIView):
     lookup_url_kwarg = 'sellrequest_id'
 
 
+
+#CART
+# class Cart(RetrieveAPIView):
+#     queryset = Cart.objects.all()
+#     serializer_class = CollectableSerializer
+#     #should it be IsOwner
+#     permission_classes = [IsAuthenticated]
+#     lookup_field='id'
+#     lookup_url_kwarg='cart_id'
+
+#     def get_queryset(self):
+#         return Cart.objects.filter(user=self.request.user)
+
+
+# class CreateCart(APIView):
+#     serializer_class = CartSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def post(self, request, *args, **kwargs):
+#         collectable = Collectable.objects.get(id=kwargs.get('collectable_id'))
+
+#         if (self.request.user.carts.filter(status=False)):
+#             cart = Cart.objects.get(user=self.request.user, status= False)
+#             if not cart.collectable.get(id=cart.id):
+#                 cart.collectable.push(cart)
+#                 cart.save()
+#         else:
+#             cart = Cart(user=self.request.user)
+#             cart.save()
+#             cart.carts.push(cart)
+#             cart.save()
+#         response = CartSerializer(CollectableSerializer).data
+#         response['collectable'] = CollectableSerializer(cart).data
+#         return Response(response)
+
+
+# class CartUpdate(RetrieveUpdateAPIView):
+#     queryset = Cart.objects.all() 
+#     serializer_class = CartSerializer
+#     lookup_field = 'id'
+#     lookup_url_kwarg = 'cart_id'
+#     permission_classes = [IsAuthenticated]
+
+
+# class CartItemDelete(DestroyAPIView):
+#     queryset = Cart.objects.all()
+#     lookup_field = 'id'
+#     lookup_url_kwarg = 'cart_id'
+#     permission_classes = [IsAuthenticated, IsCartOwner]
+
+#     def delete(self, request, *args, **kwargs):
+#         cart = self.get_object()
+#         cart = Cart.objects.filter(user=self.request.user, status=True)
+#         cart[0].cart.remove(cart)
+#         return Response({"status" : 200})
+
+
+# class Checkout (APIView):
+#     serializer_class= CartSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def get(self, request, format=None):
+#         cart = Cart.objects.get(user=self.request.user, status=False)
+#         serializer = CheckoutSerializer(cart)
+#         for collectable in cart.cart.all():
+#                 collectable.availability = False
+#                 collectable.user=self.request.user
+#                 collectable.save()
+#         cart.status=True
+#         cart.save()
+#         return Response(serializer.data)
+
+
+

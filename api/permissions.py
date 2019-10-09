0efd2b4ceaf5b29b98e5f642c4c1cdc5ca0e7757
Ahmad Helal <you@example.com>
2019-10-09 20:18:08 +0300

    create and update bids

--- api/permissions.py
+++ api/permissions.py
@@ -1,6 +1,5 @@
 from rest_framework.permissions import BasePermission
 
-
 class IsOwner(BasePermission):
 	message = "You must be the owner of this booking"
 
@@ -9,3 +8,23 @@ class IsOwner(BasePermission):
 			return True
 		else:
 			return False
+
+
+class IsNotOwner(BasePermission):
+	message = "You must be the owner of this booking"
+
+	def has_object_permission(self, request, view, obj):
+		if request.user.is_staff or (obj.user == request.user):
+			return True
+		else:
+			return False
+
+
+class IsNotOwner(BasePermission):
+	message = "You must be the owner of this booking"
+
+	def has_object_permission(self, request, view, obj):
+		if request.user.is_staff or (obj.user == request.user):
+			return False
+		else:
+			return True
--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (OnGoingBidDetail,OnGoingBidsList, BidCreateView,BidUpdateView, CreateSellRequest,DeleteSellRequest,RequestUpdateView,DeleteCollectable,CollectableList, CollectableDetails, UserCreateAPIView)
+from .views import (OnGoingBidDetail,OnGoingBidsList,BidView, CreateSellRequest,RequestUpdateView,DeleteCollectable,CollectableList, CollectableDetails, UserCreateAPIView)
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 
@@ -11,11 +11,8 @@ path('collectable/detail/<int:collectable_id>/', CollectableDetails.as_view(), n
 path('collectable/delete/<int:collectable_id>/', DeleteCollectable.as_view(), name='collectable-delete'),
 path('sellrequest/', CreateSellRequest.as_view(), name='sell-request'),
 path('sellrequest/update/<int:sellrequest_id>/', RequestUpdateView.as_view(), name='update-sellrequest'),
-path('sellrequest/delete/<int:sellrequest_id>/', DeleteSellRequest.as_view(), name='delete-sellrequest'),
 path('bid/list/', OnGoingBidsList.as_view(), name='bid-list'),
 path('bid/detail/<int:bid_id>/', OnGoingBidDetail.as_view(), name='bid-detail'),
-path('bid/create/<int:collectable_id>/', BidCreateView.as_view(), name='bid-create'),
-path('bid/update/<int:bid_id>/', BidUpdateView.as_view(), name='bid-update'),
-
+path('bid/<int:collectable_id>/', BidView.as_view(), name='bid-create'),
 ]
 
